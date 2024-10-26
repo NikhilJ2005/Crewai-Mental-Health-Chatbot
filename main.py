a18e1fcd2d0f0f5c00abb9750b3b2ebda145a4f0
Nikhil Jimmy Thomas <149070208+NikhilJ2005@users.noreply.github.com>
2024-10-26 07:18:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from crewai import Agent, Task, Crew
 from langchain_groq import ChatGroq
 import os
 from dotenv import load_dotenv
-import pysqlite3
+
+__import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
